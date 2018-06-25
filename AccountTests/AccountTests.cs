6c1ff8533cc1494037ba0131ac3a8c16cafeb462
Antoine Barbier <ABarbier@slb.com>
2018-06-25 16:42:56 +0200

    initial commit

---
+++ AccountTests/AccountTests.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Banking;
+using Xunit;
+
+namespace AccountTests
+{
+	[ExcludeFromCodeCoverage]
+    public class AccountTests
+    {
+	    [Fact]
+	    public void NewAccount_ShouldHaveBalanceEqualToZero()
+	    {
+		    IAccount newAccount = new Account(() => { });
+
+		    Assert.Equal(0, newAccount.Balance);
+	    }
+
+		[Fact]
+	    public void Calling_Deposit_OnUnverifiedAccount_ShouldAddDepositedAmountToBalance()
+		{
+			const decimal amountToDeposit = 5;
+			IAccount unverifiedAccount = new Account(() => { });
+
+			unverifiedAccount.Deposit(amountToDeposit);
+
+			Assert.Equal(amountToDeposit, unverifiedAccount.Balance);
+	    }
+
+	    [Fact]
+	    public void Calling_Withdraw_OnUnverifiedAccount_ShouldNotChangeBalance()
+	    {
+		    IAccount unverifiedAccount = new Account(() => { });
+
+			unverifiedAccount.Withdraw(5);
+
+		    Assert.Equal(0, unverifiedAccount.Balance);
+		}
+
+	    [Fact]
+	    public void Calling_Deposit_OnClosedAccount_ShouldNotChangeBalance()
+	    {
+		    IAccount closedAccount = new Account(() => { });
+			closedAccount.Close();
+
+			closedAccount.Deposit(5);
+
+		    Assert.Equal(0, closedAccount.Balance);
+	    }
+
+	    [Fact]
+	    public void Calling_Withdraw_OnClosedAccount_ShouldNotChangeBalance()
+	    {
+		    IAccount closedAccount = new Account(() => { });
+		    closedAccount.Close();
+
+			closedAccount.Deposit(5);
+
+		    Assert.Equal(0, closedAccount.Balance);
+	    }
+
+	    [Theory]
+		[InlineData(5, -5)]
+	    [InlineData(10, -10)]
+	    public void Calling_Withdraw_OnVerifiedAccount_ShouldSubtractWithdrawnAmountFromBalance(decimal amountToWithdraw, decimal resultingBalance)
+	    {
+		    IAccount account = new Account(() => { });
+			account.VerifyHolder();
+
+			account.Withdraw(amountToWithdraw);
+
+		    Assert.Equal(resultingBalance, account.Balance);
+	    }
+
+	    [Theory]
+	    [InlineData(5, 5)]
+	    [InlineData(10, 10)]
+	    public void Calling_Deposit_OnVerifiedAccount_ShouldAddDepositedAmountToBalance(decimal amountToDeposit, decimal resultingBalance)
+	    {
+		    IAccount account = new Account(() => { });
+		    account.VerifyHolder();
+
+			account.Deposit(amountToDeposit);
+
+		    Assert.Equal(resultingBalance, account.Balance);
+	    }
+
+	    [Fact]
+	    public void Calling_Withraw_OnFrozenAccount_ShouldTriggerCustomAction()
+	    {
+		    var wasCustomActionCalled = false;
+		    Action customAction = () => wasCustomActionCalled = true;
+		    IAccount account = new Account(customAction);
+		    account.VerifyHolder();
+			account.Freeze();
+
+		    account.Withdraw(5);
+
+		    Assert.True(wasCustomActionCalled);
+	    }
+
+		[Fact]
+	    public void Calling_Deposit_OnFrozenAccount_ShouldTriggerCustomAction()
+	    {
+		    var wasCustomActionCalled = false;
+		    Action customAction = () => wasCustomActionCalled = true;
+		    IAccount account = new Account(customAction);
+		    account.VerifyHolder();
+			account.Freeze();
+
+		    account.Deposit(5);
+
+		    Assert.True(wasCustomActionCalled);
+		}
+
+	    [Fact]
+	    public void Calling_Deposit_OnUnfrozenAccount_ShouldNotTriggerCustomAction()
+	    {
+		    var wasCustomActionCalled = false;
+		    Action customAction = () => wasCustomActionCalled = true;
+		    IAccount account = new Account(customAction);
+		    account.VerifyHolder();
+		    
+		    account.Deposit(5);
+
+		    Assert.False(wasCustomActionCalled);
+	    }
+
+		[Fact]
+	    public void Calling_Withraw_OnUnfrozenAccount_ShouldNotTriggerCustomAction()
+	    {
+		    var wasCustomActionCalled = false;
+		    Action customAction = () => wasCustomActionCalled = true;
+		    IAccount account = new Account(customAction);
+		    account.VerifyHolder();
+		    
+		    account.Withdraw(5);
+
+		    Assert.False(wasCustomActionCalled);
+	    }
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # BankingChallenge
 This is a suggestion challenge for the 2018 Clamart Hackathon
+
+The goal for this challenge is to refactor the code so that it **no longer contains branching instruction** (if/then/else, tertiary operator, etc), while still passing the unit test suite.
+
+The [cyclomatic complexity](https://en.wikipedia.org/wiki/Cyclomatic_complexity) should be equal to 1 for every method.
+The current stat of the metrics, as per Visual Studio 2017, is as follow:
+
+|Member|Maintainability Index|Cyclomatic Complexity|Depth of Inheritance|Class Coupling|Lines of Code
+|___|___|___|___|___|___
+Close() : void|98|1||0|1
+Deposit(decimal) : void|71|3||2|6
+Freeze() : void|98|1||0|1
+VerifyHolder() : void|98|1||0|1
+Withdraw(decimal) : void|69|4||2|6
+
+This exercise was taken from the excellent course [Making Your C# Code More Object-oriented](https://app.pluralsight.com/library/courses/c-sharp-code-more-object-oriented) by Zoran Horvat, on Plurasight.
+
+If you want to adapt it to any another language, feel free to submit a PR.
